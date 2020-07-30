e860eea98398311d06e5d61149a6bf3d232b21e3
Stovencrantz <sknapp143@gmail.com>
2020-07-30 00:11:36 -0400

    Adjusted file structure to better represent new site design and allow for proper deployment.

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,8 @@ var userScore = 0;
 var startingTimer = 1;
 var time = startingTimer * 60;
 var timerEl = document.getElementById("timer");
-var timer = setInterval(countDown, 1000);
+// var timer = setInterval(countDown, 1000);
+$(timerEl).text("Timer: " + time + "sec");
 function countDown() {
     var minutes = Math.floor(time / 60);
     var seconds = time % 60;
@@ -12,47 +13,77 @@ function countDown() {
         clearInterval(timer)
     }
     seconds = seconds < 10 ? '0' + seconds : seconds;
-    timerEl.innerHTML = `${minutes}: ${seconds}`;
+    timerEl.innerHTML = "Timer " + `${minutes}: ${seconds}`+  " sec";
     time--;
 };
 
 var questionArr = [
     {
        question: "What weight fly-line should you use for a 7-wt fly rod?", 
-       wrongAnswers: ["3lb", "8stone", "14gram", "7wt"],
+       answers: ["3lb", "8stone", "14gram", "7wt"],
        correctAnswer: "7wt"
     },
     {
        question: "What is the name of the knot that is used to tie an anglers leader to their fly line and typical calls for a piece of hardware to tie?", 
-       wrongAnswers: ["Palomar Knot", "Blood Knot", "Surgeons Knot, Nail Knot"],
+       answers: ["Palomar Knot", "Blood Knot", "Surgeons Knot, Nail Knot"],
        correctAnswer: "Nail Knot"
     },
     {
        question: "What should you do if your fly line is floating down-stream faster than your fly?", 
-       wrongAnswers: ["Watch it", "Whip it", "Nae Nae it", "Mend It"],
+       answers: ["Watch it", "Whip it", "Nae Nae it", "Mend It"],
        correctAnswer: "Mend it"
     },
     {
        question: "What is good practice to do eveytime you reach your fishing spot prior to beginning fishing?", 
-       wrongAnswers: ["Browse the sky", "Massage the earth", "Smell the air", "Read the Water"],
+       answers: ["Browse the sky", "Massage the earth", "Smell the air", "Read the Water"],
        correctAnswer: "Read the water"
     },
     {
        question: "When selecting a fly to present when fishing, what saying will help you choose the best fly for the job?", 
-       wrongAnswers: ["Evade the Grade!", "Go with the Flow!", "Sure by the Shore!", "Match the Hatch"],
+       answers: ["Evade the Grade!", "Go with the Flow!", "Sure by the Shore!", "Match the Hatch"],
        correctAnswer: "Match the Hatch!"
     }
 ];
 
 console.log(questionArr);
 
+//initialize variables to hold our card contianers
+var startCard = $("#start");
+var quizCard = $("#quiz");
+var highScoreCard = $("#highScore");
+// quizCard.css("display", "none");
+// highScoreCard.css("display", "none");
+
+
+
 var questionArea = document.querySelector("#question");
 var start = document.querySelector("#startBtn");
-
+//function to reveal our quiz card and begin the timer
 function startQuiz(){
-    console.log(questionArr[2].question);
-    // questionArea.textContent = questionArr[2].question;
-    $("#question").text(questionArr[2]);
-};
+      var index = 0;
+      $("#question").text(questionArr[index].question);
+      $(".answer1").text(questionArr[index].answers[0]);
+      console.log(questionArr[index].answers[index]);
+      $(".answer2").text(questionArr[index].answers[1]);
+      $(".answer3").text(questionArr[index].answers[2]);
+      $(".answer4").text(questionArr[index].answers[3]);
 
-startQuiz();
+      
+      // var userChoice = $(this).on
+
+      $(".answers").on("click", function(){
+
+      });
+
+   
+   }
+
+
+
+//Hide start card, show quiz card, begin quiz timer
+startCard.on("click", function(){
+   startCard.css("display", "none");
+   quizCard.css("display", "block");
+   var timer = setInterval(countDown, 1000);
+   startQuiz();
+});
